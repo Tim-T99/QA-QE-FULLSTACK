74da65b4640b2f5a15a40d6d7054a2bb10c01721
Tim <53982906+Tim-T99@users.noreply.github.com>
2025-02-17 20:08:42 +0300

    ES6

---
+++ Javascript/ES6/script.js
@@ -0,0 +1,92 @@
+const user = {
+  id: "USER-123456",
+  name: {
+    first: "Alice",
+    last: "Liddell",
+  },
+  email: "alice@example.com",
+  address: {
+    shipping: {
+      street: "123 Rabbit Hole",
+      city: "Wonderland",
+      state: "Fantasy",
+      postalCode: "12345",
+      country: "WL",
+    },
+    billing: {
+      street: "456 Mad Hatter Lane",
+      city: "Tea Party",
+      state: "Fantasy",
+      postalCode: "67890",
+      country: "WL",
+    },
+  },
+  payment: {
+    total: "100.00",
+    currency: "USD",
+    details: {
+      subtotal: "75.00",
+      tax: "15.00",
+      shipping: "10.00",
+    },
+    transactions: [
+      { id: "TXN-123", amount: "50.00", description: "Magic Potion" },
+      { id: "TXN-456", amount: "50.00", description: "Enchanted Sword" },
+    ],
+  },
+};
+
+const {
+  name: { first, last },
+  email,
+} = user;
+
+const {
+  shipping: { street, city, state, postalCode, country },
+  billing: {
+    street: street1,
+    city: city1,
+    state: state1,
+    postalCode: postalCode1,
+    country: country1,
+  },
+} = user.address;
+
+const { transactions } = user.payment;
+
+const personal = document.getElementById("personal-info");
+const allTransactions = document.getElementById("transactions");
+
+personal.innerHTML = `<h2>${first} ${last}</h2>
+                        ${email}`;
+
+const shippinginfo = document.getElementById("shipping-address");
+
+shippinginfo.innerHTML = `<h3>Shipping address</h3>
+<p><strong>Street:</strong> ${street}</p>
+<p><strong>City:</strong> ${city}</p>
+<p><strong>State:</strong> ${state}</p>
+<p><strong>Postal Code:</strong> ${postalCode}</p>
+<p><strong>Country:</strong> ${country}</p>`;
+
+const billinginfo = document.getElementById("billing-address");
+
+billinginfo.innerHTML = `<h3>Billing address</h3>
+<p><strong>Street:</strong> ${street1}</p>
+<p><strong>City:</strong> ${city1}</p>
+<p><strong>State:</strong> ${state1}</p>
+<p><strong>Postal Code:</strong> ${postalCode1}</p>
+<p><strong>Country:</strong> ${country1}</p>`;
+
+const usertransactions = transactions.map((transaction) => [
+  transaction.amount,
+  transaction.description,
+]);
+
+allTransactions.innerHTML = `<h3>Transactions</h3>
+<ul>${usertransactions
+  .map(
+    (transaction) =>
+      `<li>Amount: ${transaction[0]} Item: ${transaction[1]}</li>`
+  )
+  .join("")}</ul>`;
