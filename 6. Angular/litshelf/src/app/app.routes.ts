bf758577eb60c08363f8fb73b134da3be3f09417
Tim <53982906+Tim-T99@users.noreply.github.com>
2025-03-19 15:54:13 +0300

    litshelf

--- 6. Angular/litshelf/src/app/app.routes.ts
+++ 6. Angular/litshelf/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent }, // Root path shows welcome page//
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent }
+];
