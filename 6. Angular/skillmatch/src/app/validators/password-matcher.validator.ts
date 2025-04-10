469a4f9c6f14524d58cc62f26302d5736fc7dc10
Tim <53982906+Tim-T99@users.noreply.github.com>
2025-04-10 14:11:15 +0300

    skillmatch

--- 6. Angular/skillmatch/src/app/validators/password-matcher.validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
-
-export function passwordMatcher(passwordKey:string, confirmPasswordKey:string):ValidatorFn{
-    return (FormGroup:AbstractControl): ValidationErrors | null => {
-        const password = FormGroup.get(passwordKey)?.value;
-        const confrimPassword = FormGroup.get(confirmPasswordKey)?.value;
-        return password === confrimPassword ? null : {passwordMismatch: true}
-    }
-}
